fe5520671c546c034b21857333db47cc8c05ca7c
eidech <christopher.eide@protonmail.com>
2023-10-30 12:07:20 -0400

    RE: Issue #5 Added drop down box with CSV headers that allows user to select header to use.

--- popup.js
+++ popup.js
@@ -1,6 +1,35 @@
 document.addEventListener("DOMContentLoaded", function () {
   const csvFileInput = document.getElementById("csvFileInput");
   const runScriptButton = document.getElementById("runScript");
+  const headerList = document.getElementById("headerList");
+  const headerSelect = document.getElementById("headerSelect");
+
+  // Event listener for file input change
+  csvFileInput.addEventListener("change", function () {
+    headerList.innerHTML = ""; // Clear the header list
+    headerSelect.style.display = "none"; // Hide the select element
+
+    // Check if a file is selected
+    if (csvFileInput.files.length > 0) {
+      const file = csvFileInput.files[0];
+
+      // Read the selected file (assuming CSV)
+      const reader = new FileReader();
+      reader.onload = function (event) {
+        const csvData = event.target.result;
+        const headers = extractHeaders(csvData);
+
+        if (headers.length > 0) {
+
+          // Populate the dropdown select element with headers
+          headerSelect.innerHTML = headers.map((header) => `<option value="${header}">${header}</option>`).join('');
+          headerSelect.style.display = "block"; // Show the select element
+        }
+      };
+
+      reader.readAsText(file);
+    }
+  });
 
   runScriptButton.addEventListener("click", function () {
     console.log("Run Script Button Clicked...");
@@ -9,34 +38,38 @@ document.addEventListener("DOMContentLoaded", function () {
       console.log("CSV File Found...");
       const file = csvFileInput.files[0];
 
-      // Read the selected file (assuming CSV)
-      const reader = new FileReader();
-      reader.onload = function (event) {
-        const csvData = event.target.result;
-        const studentNumbers = extractStudentNumbers(csvData);
+      const selectedHeader = headerSelect.value; // Get the selected header
 
-        for (studentNumber in studentNumbers) {
-          console.log(studentNumber);
-        }
+      if (selectedHeader) {
+        // Read the selected file (assuming CSV)
+        const reader = new FileReader();
+        reader.onload = function (event) {
+          const csvData = event.target.result;
+          const studentNumbers = extractStudentNumbers(csvData, selectedHeader);
+
+          for (studentNumber in studentNumbers) {
+            console.log(studentNumber);
+          }
+          
+          // Send a message to your content script to trigger the action
+          chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, { action: "runScript", studentstoselect: studentNumbers });
+          });
+        };
         
-        // Send a message to your content script to trigger the action
-        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-          chrome.tabs.sendMessage(tabs[0].id, { action: "runScript", studentstoselect: studentNumbers });
-        });
-      };
-
-      reader.readAsText(file);
+        reader.readAsText(file);
+      }
     }
   });
   
   // Function to extract student numbers from the CSV
-  function extractStudentNumbers(csvData) {
+  function extractStudentNumbers(csvData, selectedHeader) {
     console.log("Extracting student numbers...")
     csvData = csvData.replace(/\r/g, '');
     const lines = csvData.split('\n');
     const headers = lines[0].split(',');
     console.log(headers);
-    const studentNumberIndex = headers.indexOf('student_studentNumber');
+    const studentNumberIndex = headers.indexOf(selectedHeader);
     
     if (studentNumberIndex === -1) {
       console.error('Header "student_studentNumber" not found in CSV.');
@@ -56,4 +89,25 @@ document.addEventListener("DOMContentLoaded", function () {
     
     return studentNumbers;
   }
+  
+  // Function to extract headers from the CSV data
+  function extractHeaders(csvData) {
+    csvData = csvData.replace(/\r/g, '');
+    const lines = csvData.split('\n');
+
+    if (lines.length === 0) {
+      console.error('CSV data is empty.');
+      return [];
+    }
+
+    const headers = lines[0].split(',');
+
+    if (headers.length === 0) {
+      console.error('No headers found in the CSV data.');
+      return [];
+    }
+
+    return headers;
+  }
+
 });
